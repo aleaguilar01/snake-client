2bc16f8c6e13407f84b758ec45c226abc8ed9e60
Alejandra Aguilar <aleaguilar01@gmail.com>
2024-03-20 21:18:00 -0600

    Adding the method to close the conection with user input: ctrl + c.

--- play.js
+++ play.js
@@ -11,3 +11,24 @@ const snake1 = connect();
 const snake2 = connect();
 
 //setInterval(() => snake2.write("Move: down"), 1000);
+
+const handleUserInput = function(key) {
+  // your code here
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  stdin.on("data", handleUserInput);
+
+
+  return stdin;
+};
+
+setupInput();
