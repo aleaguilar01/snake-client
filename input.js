ae555a92004adf24d798c0330fb5ac0e800d1106
Alejandra Aguilar <aleaguilar01@gmail.com>
2024-03-20 22:40:26 -0600

    Adding canned msn.

--- input.js
+++ input.js
@@ -8,14 +8,25 @@ const movementCommands = {
   'd': 'right'
 };
 
+const messageCommands = {
+  "h": "Hi!",
+  "b": "Bye bye!",
+  "f": "Fist-bump!",
+  "g": "Gaaaaah"
+};
+
 //Creating a function to send commands to the server
 const handleUserInput = function(key) {
   if (key === '\u0003') {
     process.exit();
   }
-  
+
   if (movementCommands[key]) {
     connection.write(`Move: ${movementCommands[key]}`);
+  }
+
+  if (messageCommands[key]) {
+    connection.write(`Say: ${messageCommands[key]}`);
   }
 };
 
