c89fb6c36e784fc8cb0b09cc249cc49f507272f4
Alejandra Aguilar <aleaguilar01@gmail.com>
2024-03-20 21:25:52 -0600

    Separating the user inputs into a new file and then importing this file into the play.js.

--- inputs.js
+++ inputs.js
@@ -1,5 +1,28 @@
+//Creating a function to close the conection using user input: ctrl + c
+const handleUserInput = function(key) {
+  // your code here
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+//Function to stablish the parameters of the inputs.
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  stdin.on("data", handleUserInput);
+
+
+  return stdin;
+};
+
 const moveSnakeUp = (snake) => {
   setInterval(() => snake.write("Move: up"), 1000);
 };
 
 module.exports = moveSnakeUp;
+
+module.exports = setupInput;
--- play.js
+++ play.js
@@ -1,5 +1,6 @@
 const connect = require("./client.js");
 const moveSnakeUp = require("./inputs.js");
+const setupInput = require("./inputs.js");
 
 console.log("Connecting ...");
 
@@ -12,23 +13,7 @@ const snake2 = connect();
 
 //setInterval(() => snake2.write("Move: down"), 1000);
 
-const handleUserInput = function(key) {
-  // your code here
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  stdin.on("data", handleUserInput);
 
 
-  return stdin;
-};
 
 setupInput();
